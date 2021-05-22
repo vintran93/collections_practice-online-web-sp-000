b941125e7883ae3cad5d4e63bc1168414e066ad6
Vincent Tran <vincentuong@yahoo.com>
2021-05-22 18:40:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(numbers)
+  numbers.sort
+end
 
+def sort_array_desc(numbers)
+  numbers.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort {|a,b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+  array.sort{|a,b| a[1]<=>b[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect {|arr| arr.delete(arr[2]).insert(2,"$")}
+end 
+
+def find_a(array)
+  array.keep_if {|arr| arr[0]=="a"}
+end 
+
+def sum_array(array)
+  array.inject(0, :+)
+end
+
+def add_s(array)
+  array.collect{|arr| arr==array[1] ? arr:arr <<"s"}
+end
